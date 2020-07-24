a20830e2941cc6da2843ea41004557b7b0097d60
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-07-24 17:44:20 -0400

    Added a new lexer file to start testing

---
+++ simpletex/src/main/scala/Parser/Lexer.scala
@@ -0,0 +1,7 @@
+/**
+  * This file contains the scanner of the parser we are building
+  *
+  * We start with a scanner that uses regular expressions to recognize the
+  * language and tokenize it. The following phase will be the responsbility
+  * of the grammar implemented in the parser2 file.
+  */
