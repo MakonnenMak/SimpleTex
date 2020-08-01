9b2aab1c5cf51e5fd1bed27194d60f76ee7e4ac0
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-08-01 13:51:46 -0400

    Another test case

--- simpletex/src/test/scala/LexerTest.scala
+++ simpletex/src/test/scala/LexerTest.scala
@@ -1,14 +1,16 @@
 package simpletex.lexer;
-import org.scalatest.FunSuite;
+import org.scalatest.funsuite.AnyFunSuite
+
 import simpletex.lexer._;
 
-class LexerSectionTests extends FunSuite {
+class LexerSectionTests extends AnyFunSuite {
   test("A section on a single line should produce a SECTION object") {
-    assert(
-      SimpleTexLexer("# section name \n") match {
-        case Left(value)  => false
-        case Right(value) => true
-      }
-    )
+    SimpleTexLexer("# section name \n") match {
+      case Left(value) =>
+        assert(false, "Didn't parse the section out of the string")
+      case Right(value :: Nil) =>
+        assert(value.equals(SECTION("section name \n")))
+      case Right(_) => assert(false, "We returned more than one section")
+    }
   }
 }
