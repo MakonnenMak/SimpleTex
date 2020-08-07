aaaeab12d835778e6ce855c0dd913f00d6d54c6b
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-08-07 17:42:15 -0400

    Start of Issue #9
    The next few commits will add integration tests

---
+++ simpletex/src/test/scala/LexerIntegration.scala
@@ -0,0 +1,6 @@
+package simpletex.lexer
+import org.scalatest.funsuite.AnyFunSuite
+import simpletex.lexer._
+
+// TODO use the word integration in the test names
+class LexerIntegrationSection extends AnyFunSuite {}
