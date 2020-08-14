9e7e9565229f5555e089f08c48f30770d1e5031a
MakonnenMak <solmak11@gmail.com>
2020-08-14 00:18:34 -0400

    Section integration tests * Section bold * Section italics * Section bold italics * Section refs

--- simpletex/src/test/scala/LexerIntegration.scala
+++ simpletex/src/test/scala/LexerIntegration.scala
@@ -24,4 +24,105 @@ class LexerIntegrationHeaderTests extends AnyFunSuite {
   }
 }
 
-class LexerIntegrationTextFormatTests extends AnyFunSuite {}
+// TODO Some short naming convention for this..
+class LexerIntegrationSectionTextFormatTests extends AnyFunSuite {
+  test("A section with a bold piece of text below it") {
+    SimpleTexLexer("# section name \n **Hello World!**") match {
+      case Left(value) =>
+        assert(
+          false,
+          "Didn't parse the section and bold text out of the string"
+        )
+      case Right(
+            List(SECTION("section name \n"), BOLD("Hello World!"))
+          ) =>
+        assert(true)
+      case Right(_) =>
+        assert(
+          false,
+          "We returned more than a section and a bold piece of text"
+        )
+    }
+  }
+
+  test("A section with a italisized piece of text below it") {
+    SimpleTexLexer("# section name \n *Fancy italic text*") match {
+      case Left(value) =>
+        assert(
+          false,
+          "Didn't parse the section and italisized text out of the string"
+        )
+      case Right(
+            List(SECTION("section name \n"), ITALICS("Fancy italic text"))
+          ) =>
+        assert(true)
+      case Right(_) =>
+        assert(
+          false,
+          "We returned more than a section and an italisized piece of text"
+        )
+    }
+  }
+
+  test("A section with a bold+italisized piece of text below it") {
+    SimpleTexLexer("# section name \n ***Bold and fancy italics***") match {
+      case Left(value) =>
+        assert(
+          false,
+          "Didn't parse the section and bold+italisized text out of the string"
+        )
+      case Right(
+            List(
+              SECTION("section name \n"),
+              BOLDITALICS("Bold and fancy italics")
+            )
+          ) =>
+        assert(true)
+      case Right(_) =>
+        assert(
+          false,
+          "We returned more than a section and a bold+italisized piece of text"
+        )
+    }
+  }
+
+  test("A section with a reference below it") {
+    SimpleTexLexer("# section name \n @ref{some label} ") match {
+      case Left(value) =>
+        assert(
+          false,
+          "Didn't parse the section and reference out of the string"
+        )
+      case Right(
+            List(SECTION("section name \n"), REFERENCE("some label"))
+          ) =>
+        assert(true)
+      case Right(_) =>
+        assert(
+          false,
+          "We returned more than a section and a reference"
+        )
+    }
+  }
+
+  test("A section with a citation below it") {
+    SimpleTexLexer(
+      "# section name \n @cite{some citation} "
+    ) match {
+      case Left(value) =>
+        assert(
+          false,
+          "Didn't parse the section and citation out of the string"
+        )
+      case Right(
+            List(SECTION("section name \n"), CITATION("some citation"))
+          ) =>
+        assert(true)
+      case Right(_) =>
+        assert(
+          false,
+          "We returned more than a section and a citation"
+        )
+    }
+  }
+}
