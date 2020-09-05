746aa61378bc7d71d0915667104bdd60767f9bef
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-09-05 18:35:54 -0400

    added lexer to parser transformers

--- simpletex/src/main/scala/Parser/Parser.scala
+++ simpletex/src/main/scala/Parser/Parser.scala
@@ -35,15 +35,44 @@ object SimpleTexParser extends Parsers {
 
   def sections: Parser[SimpleTexAST] = {
     val mainSection =
-      section ~ rep(subsection) ~ rep(content) ^^ { //TODO subsection should be a parser not lexer subsection
+      section ~ rep(mainSubsection) ~ rep(content) ^^ {
         case SECTION(title) ~ subsections ~ contents =>
           Section(title, subsections, contents)
       }
-    val sectionLayout = layout ~ section ^^ {
+    val sectionLayout = layout ~ mainSection ^^ {
       case LAYOUT(layout) ~ mainSection =>
         LayoutSection(layout, mainSection)
     }
-    section | subsection | layout
+
+    val content =???
+
+    val mainSubsection = subsection ~ rep(content) ^^ {
+      case SUBSECTION(title) ~ contents => Subsection(title, contents)
+    }
+
+    mainSubsection
+  }
+
+  private def text: Parser[CONTENT] = { // TODO should consider renaming content and plaintext ==> same idea but different names
+    accept("plaintext", { case s @ CONTENT(name) => s })
+  }
+  private def bold: Parser[BOLD] = {
+    accept("bold", { case s @ BOLD(name) => s })
+  }
+  private def italics: Parser[ITALICS] = {
+    accept("italics", { case s @ ITALICS(name) => s })
+  }
+  private def bolditalics: Parser[BOLDITALICS] = {
+    accept("bolditalics", { case s @ BOLDITALICS(name) => s })
+  }
+  private def citation: Parser[CITATION] = {
+    accept("citation", { case s @ CITATION(name) => s })
+  }
+  private def reference: Parser[REFERENCE] = {
+    accept("reference", { case s @ REFERENCE(name) => s })
+  }
+  private def image: Parser[IMAGE] = {
+    accept("image", { case s @ IMAGE(name, caption, path) => s })
   }
 
   private def section: Parser[SECTION] = {
