b390184ffec956113099f192ab91fb6b945a2930
MakonnenMak <solmak11@gmail.com>
2020-09-13 19:19:16 -0400

    updated parser test

---
+++ simpletex/src/test/scala/ParserTest.scala
@@ -0,0 +1,31 @@
+package simpletex.lexer;
+import org.scalatest.funsuite.AnyFunSuite
+
+import simpletex.lexer._;
+import simpletex.parser._;
+
+class BasicParser extends AnyFunSuite {
+  test("Parsing a section") {
+    SimpleTexParser(
+      Seq(
+        SECTION(),
+        TEXT("hi"),
+        TEXT("world"),
+        NEWLINE(),
+        TEXT("fun"),
+        TEXT("yayy"),
+        SECTION(),
+        TEXT("Dog"),
+        TEXT("cat"),
+        NEWLINE(),
+        TEXT("woof"),
+        TEXT("woof")
+      )
+    ) match {
+      case Left(e) =>
+        fail("Didn't parse the left symbol for bold italics at all")
+      case Right(v) =>
+        assert(true, s"$v")
+    }
+  }
+}
