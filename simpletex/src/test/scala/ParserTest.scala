e3aacb9adcf196d65fac0110e911c8a569e9b553
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-09-19 19:10:46 -0400

    Basic test for now. need to add more

--- simpletex/src/test/scala/ParserTest.scala
+++ simpletex/src/test/scala/ParserTest.scala
@@ -25,7 +25,41 @@ class BasicParser extends AnyFunSuite {
       case Left(e) =>
         fail(s"Didn't parse the left symbol for bold italics at all: $e")
       case Right(v) =>
-        assert(true, s"$v")
+        assert(true)
+    }
+  }
+
+  test("Bold should parse correctly") {
+    SimpleTexParser(
+      Seq(
+        BOLDL(),
+        TEXT("hello"),
+        TEXT("world"),
+        TEXT("hi"),
+        BOLDR()
+      )
+    ) match {
+      case Left(value)  => fail(s"We didn't parse this correctly: $value")
+      case Right(value) => assert(true)
+    }
+  }
+
+  test("Section with bold content parses") {
+    SimpleTexParser(
+      Seq(
+        SECTION(),
+        TEXT("hi"),
+        TEXT("world"),
+        NEWLINE(),
+        BOLDL(),
+        TEXT("hello"),
+        TEXT("world"),
+        TEXT("hi"),
+        BOLDR()
+      )
+    ) match {
+      case Left(value)  => fail(s"We didn't parse this correctly: $value")
+      case Right(value) => assert(true)
     }
   }
 }
