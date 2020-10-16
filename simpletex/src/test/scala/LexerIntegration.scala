7ca9e0da62fe8576a88bbdba2b15bc6327dec093
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-10-15 22:19:20 -0400

    All section integration test cases. Close Issue #9

--- simpletex/src/test/scala/LexerIntegration.scala
+++ simpletex/src/test/scala/LexerIntegration.scala
@@ -3,7 +3,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import simpletex.lexer._
 
 // TODO use the word I in the test names
-class ISection extends AnyFunSuite {
+class ISectionTextModifers extends AnyFunSuite {
   test("with a plaintext title should parse correctly") {
     SimpleTexLexer("# section name \n") match {
       case Left(value) =>
@@ -66,6 +66,173 @@ class ISection extends AnyFunSuite {
         assert(true)
       case Right(_) =>
         assert(false, "We returned the wrong structure or a non section")
+    }
+  }
+
+  test("with plaintext and bold should parse") {
+    SimpleTexLexer(
+      "# section name \n plaintext mixed with /** bold items **/ should parse"
+    ) match {
+      case Left(value) => fail("Didn't parse the mixed content correctly")
+      case Right(
+          List(
+            SECTION(),
+            TEXT("section"),
+            TEXT("name"),
+            NEWLINE(),
+            TEXT("plaintext"),
+            TEXT("mixed"),
+            TEXT("with"),
+            BOLDL(),
+            TEXT("bold"),
+            TEXT("items"),
+            BOLDR(),
+            TEXT("should"),
+            TEXT("parse")
+          )
+          ) =>
+        assert(true)
+      case Right(wrong) => assert(false, s"We returned something else: $wrong")
+    }
+  }
+  test("with plaintext and bolditalics should parse") {
+    SimpleTexLexer(
+      "# section name \n plaintext mixed with /*** bolditalics items ***/ should parse"
+    ) match {
+      case Left(value) => fail("Didn't parse the mixed content correctly")
+      case Right(
+          List(
+            SECTION(),
+            TEXT("section"),
+            TEXT("name"),
+            NEWLINE(),
+            TEXT("plaintext"),
+            TEXT("mixed"),
+            TEXT("with"),
+            BOLDITALICSL(),
+            TEXT("bolditalics"),
+            TEXT("items"),
+            BOLDITALICSR(),
+            TEXT("should"),
+            TEXT("parse")
+          )
+          ) =>
+        assert(true)
+      case Right(wrong) => assert(false, s"We returned something else: $wrong")
+    }
+  }
+  test("with plaintext and italics should parse") {
+    SimpleTexLexer(
+      "# section name \n plaintext mixed with /* italics items */ should parse"
+    ) match {
+      case Left(value) => fail("Didn't parse the mixed content correctly")
+      case Right(
+          List(
+            SECTION(),
+            TEXT("section"),
+            TEXT("name"),
+            NEWLINE(),
+            TEXT("plaintext"),
+            TEXT("mixed"),
+            TEXT("with"),
+            ITALICSL(),
+            TEXT("italics"),
+            TEXT("items"),
+            ITALICSR(),
+            TEXT("should"),
+            TEXT("parse")
+          )
+          ) =>
+        assert(true)
+      case Right(wrong) => assert(false, s"We returned something else: $wrong")
+    }
+  }
+}
+
+class ISectionRefs extends AnyFunSuite {
+  test("with plaintext and references should parse") {
+    SimpleTexLexer(
+      "# section name \n plaintext mixed with @ref{ some label } more plaintext"
+    ) match {
+      case Left(value) => fail("Didn't parse the mixed content correctly")
+      case Right(
+          List(
+            SECTION(),
+            TEXT("section"),
+            TEXT("name"),
+            NEWLINE(),
+            TEXT("plaintext"),
+            TEXT("mixed"),
+            TEXT("with"),
+            REFERENCE(),
+            BRACEL(),
+            TEXT("some"),
+            TEXT("label"),
+            BRACER(),
+            TEXT("more"),
+            TEXT("plaintext")
+          )
+          ) =>
+        assert(true)
+      case Right(wrong) => assert(false, s"We returned something else: $wrong")
+    }
+  }
+
+  test("with plaintext and equations should parse") {
+    SimpleTexLexer(
+      "# section name \n plaintext mixed with /$ 1 + 1 = 2 $/ more plaintext"
+    ) match {
+      case Left(value) => fail("Didn't parse the mixed content correctly")
+      case Right(
+          List(
+            SECTION(),
+            TEXT("section"),
+            TEXT("name"),
+            NEWLINE(),
+            TEXT("plaintext"),
+            TEXT("mixed"),
+            TEXT("with"),
+            EQUATIONL(),
+            TEXT("1"),
+            TEXT("+"),
+            TEXT("1"),
+            TEXT("="),
+            TEXT("2"),
+            EQUATIONR(),
+            TEXT("more"),
+            TEXT("plaintext")
+          )
+          ) =>
+        assert(true)
+      case Right(wrong) => assert(false, s"We returned something else: $wrong")
+    }
+  }
+
+  test("with plaintext and citations should parse") {
+    SimpleTexLexer(
+      "# section name \n plaintext mixed with @cite{ citations here } more plaintext"
+    ) match {
+      case Left(value) => fail("Didn't parse the mixed content correctly")
+      case Right(
+          List(
+            SECTION(),
+            TEXT("section"),
+            TEXT("name"),
+            NEWLINE(),
+            TEXT("plaintext"),
+            TEXT("mixed"),
+            TEXT("with"),
+            CITATION(),
+            BRACEL(),
+            TEXT("citations"),
+            TEXT("here"),
+            BRACER(),
+            TEXT("more"),
+            TEXT("plaintext")
+          )
+          ) =>
+        assert(true)
+      case Right(wrong) => assert(false, s"We returned something else: $wrong")
     }
   }
 }
