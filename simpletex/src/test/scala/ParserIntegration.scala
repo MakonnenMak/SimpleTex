b87433dd499ec58ce1ab5e15d068f48963fa8eda
MakonnenMak <solmak11@gmail.com>
2020-10-17 13:54:30 -0400

    Large test case for parser section integration with layout

--- simpletex/src/test/scala/ParserIntegration.scala
+++ simpletex/src/test/scala/ParserIntegration.scala
@@ -4,9 +4,54 @@ import org.scalatest.funsuite.AnyFunSuite
 import simpletex.lexer._;
 import simpletex.parser._;
 
-class ParserSectionIntegration extends AnyFunSuite {
-  test("some test") {
-    assert(true)
+class ParserSectionIntegrationLarge extends AnyFunSuite { //idk what to name the class
+  test("Section with layout and a lot of other content") {
+    SimpleTexParser(
+      Seq(
+        LAYOUT(),
+        TEXT("l1"),
+        NEWLINE(),
+        SECTION(),
+        TEXT("Intro"),
+        TEXT("Section"),
+        NEWLINE(),
+        BOLDITALICSL(),
+        TEXT("hello"),
+        TEXT("world"),
+        BOLDITALICSR(), //FIX: adding a new line under here will break this test
+        TEXT("this"),
+        TEXT("is"),
+        TEXT("an"),
+        BOLDL(),
+        TEXT("important"),
+        BOLDR(),
+        ITALICSL(),
+        TEXT("paper"),
+        ITALICSR(),
+        TEXT("equations"),
+        EQUATIONL(),
+        TEXT("4x^2+5"),
+        EQUATIONR(),
+        TEXT("citations"),
+        CITATION(),
+        BRACEL(),
+        TEXT("Some"),
+        TEXT("Citation"),
+        BRACER(),
+        TEXT("Some"),
+        TEXT("Reference"),
+        REFERENCE(),
+        BRACEL(),
+        TEXT("Some"),
+        TEXT("Reference"),
+        BRACER()
+      )
+    ) match {
+      case Left(value) =>
+        fail(s"We didn't parse this correctly: $value")
+      case Right(value) =>
+        assert(true)
+    }
   }
 }
 class ParserSubsectionIntegration extends AnyFunSuite {
