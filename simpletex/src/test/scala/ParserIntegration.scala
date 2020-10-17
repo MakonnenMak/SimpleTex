2b3e000f240c3bf60488da51fa3be34ebdc00969
MakonnenMak <solmak11@gmail.com>
2020-10-17 14:09:49 -0400

    subsection with plaintext parser test

--- simpletex/src/test/scala/ParserIntegration.scala
+++ simpletex/src/test/scala/ParserIntegration.scala
@@ -56,7 +56,22 @@ class ParserSectionIntegrationLarge extends AnyFunSuite { //idk what to name the
 }
 class ParserSubsectionIntegration extends AnyFunSuite {
   test("subsection with plaintext") {
-    assert(true)
+    SimpleTexParser(
+      Seq(
+        SECTION(),
+        TEXT("Section"),
+        NEWLINE(),
+        SUBSECTION(),
+        TEXT("my"),
+        TEXT("subsection"),
+        NEWLINE(),
+        TEXT("hello"),
+        TEXT("world")
+      )
+    ) match {
+      case Left(value)  => fail(s"We didn't parse this correctly: $value")
+      case Right(value) => assert(true)
+    }
   }
   test("subsection with large plaintext") {
     assert(true)
