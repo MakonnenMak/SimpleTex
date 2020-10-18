7170264260ddc681a9d0aa46132fb0c5ba84ccee
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-10-18 18:04:01 -0400

    Added the compiler chain

--- simpletex/src/main/scala/Compiler/SimpleTexCompiler.scala
+++ simpletex/src/main/scala/Compiler/SimpleTexCompiler.scala
@@ -1,4 +1,14 @@
 package simpletex.compiler
+
 import simpletex.lexer.SimpleTexLexer
 import simpletex.parser.{SimpleTexParser, SimpleTexAST}
 
+object SimpleTexCompiler {
+  def apply(code: String): Either[SimpleTexCompilationError, SimpleTexAST] = {
+    for {
+      tokens <- SimpleTexLexer(code).right
+      ast <- SimpleTexParser(tokens).right
+    } yield ast
+  }
+
+}
