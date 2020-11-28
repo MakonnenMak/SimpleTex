a898582c7e3198591340b4282a9d879bc8f5a594
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2020-11-27 19:16:51 -0500

    filling in the document class def

--- simpletex/src/main/scala/Generator/Document.scala
+++ simpletex/src/main/scala/Generator/Document.scala
@@ -2,12 +2,17 @@
  *
  * The document contains a set of layouts. Exposes a way to update a specific cell in a specific layout, which is used by the code generating functions. Eventually, the user calls `generateDocument` to generate the latex document.
  */
-
 package Generator
 
-class Document(layout: String) {
+import collection.mutable.Map
+
+//TODO define layout class that has map: cell -> string
+class Document(layout: List[String]) {
   override def toString = "Some document place holder"
 
-  def update(cellid:Int, content:String): Unit =???
-  def generateDocument():String = ???
+  def update(layoutid: Int, cellid: Int, content: String): Unit = {}
+  def generateDocument(): String = { "not implemented" }
+
+  //TODO second string is layout objects
+  private val layouts: Map[String, String] = Map()
 }
