2643892624fefca8b524f5dafd1020048a92f857
MakonnenMak <solmak11@gmail.com>
2020-11-29 16:46:41 -0500

    Document Generator Skeleton

---
+++ simpletex/src/main/scala/Generator/DocumentGenerators.scala
@@ -0,0 +1,10 @@
+package simpletex.generator
+import simpletex.generator.Document
+import simpletex.compiler.SimpleTexCompiler
+import simpletex.parser.SimpleTexAST
+object DocumentGenerator {
+  def apply(ast: SimpleTexAST, document: Document): String = {
+    "Not implemented"
+  }
+
+}
