3ddc494267de6e80e38cec67e2930e4cb3b4c36f
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2021-01-31 16:53:13 -0500

    Some progress on the document object. mostly notes

--- simpletex/src/main/scala/Generator/Document.scala
+++ simpletex/src/main/scala/Generator/Document.scala
@@ -6,13 +6,14 @@ package simpletex.generator
 
 import collection.mutable.Map
 import collection.mutable.Queue
+import simpletex.compiler.{SimpleTexCompilationError, SimpleTexGeneratorError}
 
 //TODO define layout class that has map: cell -> string
 class Document(layout: List[Layout]) {
   override def toString = "Some document place holder"
 
   def fillLayoutKeys(): Unit = {
-    layout.map(l => layouts.put(l.name, Map()))
+    layout.foreach(l => layouts.put(l.name, Map()))
   }
 
   def update(layoutID: String, cellID: String, content: String): Unit = {
@@ -23,11 +24,17 @@ class Document(layout: List[Layout]) {
     }
   }
   def generateDocument(): Either[String, String] = { Left("not implemented") }
-
+  // ast map (BOLD => "this is bold") ==> all bold nodes should be "this..."
+  // ast map (Section => "section ... children") ==> all sections and their children pass through the function
+  //      ^^^ subsection should never pass through the function
   fillLayoutKeys()
 
-  private def processLayout(layout: String): Either[String, String] = {
-    Left("not implemented")
+  /* Called by `generateDocument` to process a single layout object.
+   */
+  private def processLayout(
+      layout: String
+  ): Either[SimpleTexCompilationError, String] = {
+    Left(SimpleTexGeneratorError("not implemented"))
   }
   private val accessQueue: Queue[String] = Queue()
   private val layouts: Map[String, Map[String, String]] = Map()
