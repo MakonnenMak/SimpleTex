d1dc57cce564643523c3d8473893c992225afe5b
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2021-02-28 16:07:05 -0500

    Finalizing the AST generator
    Combined the ast generator with the document generator. The doc gen now
    handles the ast generation as a function that takes the document and the
    ast. Still untested and design might need some updates.

--- simpletex/src/main/scala/Generator/DocumentGenerators.scala
+++ simpletex/src/main/scala/Generator/DocumentGenerators.scala
@@ -1,5 +1,5 @@
 package simpletex.generator
-import simpletex.generator.{LatexDocument, LatexConstant}
+import simpletex.generator.LatexDocument
 import simpletex.compiler.{
   SimpleTexCompiler,
   SimpleTexCompilationError,
@@ -7,69 +7,52 @@ import simpletex.compiler.{
 }
 import simpletex.parser._
 
-object Generator {
-  def generateAST(node: SimpleTexAST) = {
-    // decide what to do here
-  }
-
-  private def joinWithBraces(
-      construct: String,
-      content: String
-  ): String =
-    LatexConstant.lBrace + content.mkString(" ") + LatexConstant.rBrace
-
-  def generateContent(node: Content): String = node match {
-    case PlainText(text) => text.mkString
-    case Bold(text)      => joinWithBraces("\textbf", generateContent(text))
-    case Italics(text)   => joinWithBraces("\textit", generateContent(text))
-    case BoldItalics(text) =>
-      generateContent(Bold(PlainText(Seq(generateContent(Italics(text))))))
-    case Citation(text)  => joinWithBraces("""\cite""", generateContent(text))
-    case Reference(text) => joinWithBraces("""\ref""", generateContent(text))
-    case Image(caption, path) =>
-      s"\\begin{figure}[h] \\centering \\includegraphics[width=0.8\\linewidth]{${generateContent(path)}} \\caption{${generateContent(caption)}}% \\label{fig:{caption.subString(0,5)}} \\end{figure}"
-    case Equation(text) => "$" + generateContent(text) + "$"
-    case Newline()      => "\n" //TODO do we want an empty line or just a new line?
-  }
-  def apply(node: SimpleTexAST): String = node match {
-    case Document(body) =>
-      body
-        .map(Generator(_))
-        .mkString // do we want to convert to a string this way
-    case Section(name, subsection, content) =>
-      // TODO add in the section name properly
-      val subs = subsection.map(Generator(_)).mkString
-      val sectionBody: String = content.map(generateContent(_)).mkString
-      name + sectionBody + subs
-    case Subsection(name, content) =>
-      name + content.map(generateContent(_)).mkString
-    case PlainBody(content) =>
-      content.map(generateContent(_)).mkString
-    case LayoutSection(name, section) =>
-      name + Generator(section)
-  }
-}
 object DocumentGenerator {
   def apply(
       ast: SimpleTexAST,
       layout: List[Layout]
   ): Either[SimpleTexCompilationError, LatexDocument] = {
     val doc = LatexDocument(layout)
+
     Left(SimpleTexGeneratorError("not implemented"))
   }
+  def generateContent(node: Content): String = node match {
+    case PlainText(text) => text.mkString(" ")
+    case Bold(text)      => s"\textbf{${generateContent(text)}}"
+    case Italics(text)   => s"\textit{${generateContent(text)}}"
+    case BoldItalics(text) =>
+      generateContent(Bold(PlainText(Seq(generateContent(Italics(text))))))
+    case Citation(text)  => s"\\cite{${generateContent(text)}}"
+    case Reference(text) => s"\\ref${generateContent(text)}}"
+    case Image(caption, path) =>
+      s"\\begin{figure}[h] \\centering \\includegraphics[width=0.8\\linewidth]{${generateContent(path)}} \\caption{${generateContent(caption)}}% \\label{fig:{caption.subString(0,5)}} \\end{figure}"
+    case Equation(text) => "$" + generateContent(text) + "$"
+    case Newline()      => "\n" //TODO do we want an empty line or just a new line?
+  }
+  def generateAST(doc: LatexDocument)(node: SimpleTexAST): String = {
+    val generator: SimpleTexAST => String = generateAST(doc)
+    node match {
+      case Document(body) =>
+        body
+          .map(generator(_))
+          .mkString // do we want to convert to a string this way
+      case Section(name, subsection, content) =>
+        val subs = subsection.map(generator(_)).mkString
+        val sectionBody: String =
+          content.map(generateContent(_)).mkString + "\n"
+        "\\section{" + name + "}\n" + sectionBody + subs
+      case Subsection(name, content) =>
+        "\\subsection{" + name + "}" + content
+          .map(generateContent(_))
+          .mkString + "\n"
+      case PlainBody(content) =>
+        content.map(generateContent(_)).mkString
+      case LayoutSection(name, section) =>
+        generateContent(name).split(".") match {
+          case Array(a, b) => doc.update(a, b, generator(section))
+          case array       => s"WRONG TYPE: $array"
+        }
+    }
+  }
 
-  /* def bold ..
-   * def italics ...
-   *
-   *
-   */
-  /* def section(...) match
- *
- * bold:  -> bold(content) or produce here \bf ...
- * italics: -> \it...
- *
- *
- *
- *
- */
 }
