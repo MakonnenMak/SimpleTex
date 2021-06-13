7682425f1f30eb5cb6b1a27bb62dac6930c6c3ae
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2021-06-13 16:13:09 -0400

    Work on layout parsing

--- simpletex/src/main/scala/Generator/Layout.scala
+++ simpletex/src/main/scala/Generator/Layout.scala
@@ -13,4 +13,27 @@ case class Layout(
   override def toString(): String =
     f"Name: ${name} \n Columns: ${colSizes} \n Rows: ${rowSizes} \n Cell Names: ${cellNames}"
 
+  def convertCol(): List[Int] = colSizes.map(x => colSizes.size())
+  def convertRow(): List[Int] = rowSizes.map(x => (1 / x) * 100)
+// (x / sum(x)) * 100
+//  33  33  33
+// .5   .25  .25
+//  2    1    1
+//  list.size
+//
+//
+//  .5    .5
+//  1      1
+//  |  .5   | .5  ---> colSizes.size --> number of cols
+//
+//  12 # of cols
+//  .5   .5
+//  6    6  <-- 12 * .5
+//  12 * .33
+//
+//  .5 25 25 -> 3
+//  3*.5 --> 1.5 --> 2
+//  3* .25 --> .75 -->1
+//  3* .25 --> .775 --> 1
+
 }
